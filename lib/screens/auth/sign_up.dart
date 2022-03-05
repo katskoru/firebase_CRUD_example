cf224f6b81bc592ece7364738463a7a441d41fd9
katskoru <katskoru@gmail.com>
2022-03-05 12:54:12 +0100

    first commit

---
+++ lib/screens/auth/sign_up.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                const FlutterLogo(size: 100.0),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: "Email"),
+                    controller: _emailController,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(labelText: "Password"),
+                    controller: _passwordController,
+                    obscureText: true,
+                  ),
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Sign Up"),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
