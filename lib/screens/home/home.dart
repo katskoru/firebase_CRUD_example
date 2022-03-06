cb36a35b3b86080ba1e20782a554e912cbd823b9
katskoru <katskoru@gmail.com>
2022-03-06 12:44:26 +0100

    lista

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_example/screens/auth/data/providers/auth_state.dart';
+import 'package:firebase_example/screens/home/data/models/cars_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,11 +20,29 @@ class Home extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return listItem("Brand", index.toString());
+          child: FutureBuilder(
+              future: FirebaseFirestore.instance.collection("cars").get(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  List<Cars> _listOfCars =
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Cars.fromJson(data);
+                  }).toList();
+
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: _listOfCars.length,
+                      itemBuilder: (context, index) {
+                        Cars _car = _listOfCars[index];
+                        return listItem(_car.brand, _car.model);
+                      });
+                }
               }),
         ),
       ),
@@ -30,10 +50,17 @@ class Home extends StatelessWidget {
   }
 
   Widget listItem(brand, model) {
-    return ListTile(
-      title: Text(brand),
-      subtitle: Text(model),
-      trailing: IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        if (direction == DismissDirection.startToEnd ||
+            direction == DismissDirection.endToStart) print("delete");
+      },
+      child: ListTile(
+        title: Text(brand),
+        subtitle: Text(model),
+        trailing: IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+      ),
     );
   }
 }
